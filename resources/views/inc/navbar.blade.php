4b0c5ff9148fd5ccd3752f8632efbfb28425b128
ilyaasp <ilyas.hammam17@gmail.com>
2020-03-07 01:00:37 +0700

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">TodoList</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="{{Request::is('/') ? 'active' : ''}}"><a href="/">Home</a></li>
+                <li class="{{Request::is('todo/create') ? 'active' : ''}}"><a href="todo/create">Create Todo</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
